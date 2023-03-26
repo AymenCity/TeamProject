80bea38518040652d9847d523753d6e113d2c450
AymenCity <91978242+AymenCity@users.noreply.github.com>
2023-03-26 23:59:29 +0100

    Updated Login & Signup - improved design layout, added functionality to buttons (V1.2 - Aymen)

--- src/Login.java
+++ src/Login.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Login extends JFrame {
     private JPanel mainPanel;
@@ -7,19 +9,31 @@ public class Login extends JFrame {
     private JTextField emailTextField;
     private JLabel passwordLabel;
     private JTextField passwordTextField;
-    private JButton signupButton;
+    private JButton signUpButton;
     private JButton loginButton;
     private JLabel orLabel;
     private JLabel infoLabel;
 
+    public Login() {
+        setContentPane(mainPanel);
+        setTitle("ATS System");
+        setSize(600,300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+
+        signUpButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                Signup signup = new Signup();
+            }
+        });
+    }
+
     public static void main(String[] args) {
         System.out.println("Booting up... ATS System");
         Login login = new Login();
-        login.setContentPane(login.mainPanel);
-        login.setTitle("ATS System");
-        login.setSize(600,300);
-        login.setVisible(true);
-        login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
 
--- src/Signup.java
+++ src/Signup.java
@@ -1,15 +1,38 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Signup {
+public class Signup extends JDialog {
     private JPanel mainPanel;
     private JLabel signupLabel;
     private JLabel infoLabel;
     private JTextField nameTextField;
     private JTextField emailTextField;
     private JTextField passwordTextField;
-    private JCheckBox acceptCheckBox;
-    private JButton signupButton;
+    private JButton signUpButton;
     private JLabel nameLabel;
     private JLabel emailLabel;
     private JLabel passwordLabel;
+    private JRadioButton iAcceptTheTermsRadioButton;
+    private JButton cancelButton;
+
+    public Signup() {
+        setContentPane(mainPanel);
+        setTitle("ATS System");
+        setSize(600,300);
+        setVisible(true);
+
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                Login login = new Login();
+            }
+        });
+    }
+
+    public JPanel getMainPanel(){
+        return mainPanel;
+    }
 }
+
