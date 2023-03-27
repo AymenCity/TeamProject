55cbfd2c8cb31ce52b87e66691319526f920c4d4
AymenCity <91978242+AymenCity@users.noreply.github.com>
2023-03-27 15:10:14 +0100

    Aymen - added UserManagement

---
+++ src/UserManagement.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class UserManagement {
+    private JButton newButton;
+    private JButton deleteButton;
+    private JButton cancelButton;
+    private JButton applyButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+}
