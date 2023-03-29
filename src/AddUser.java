c9eeda65d97d160ea7b74fbbb006898fee6d547d
EthanBrewerCity <91800294+EthanBrewerCity@users.noreply.github.com>
2023-03-29 13:34:15 +0100

    Ethan - 29/03/23 - Fixed error with refund button on welcome page

--- src/AddUser.java
+++ src/AddUser.java
@@ -2,4 +2,9 @@ import javax.swing.*;
 
 public class AddUser {
     private JPanel mainPanel;
+    private JLabel fullNameLabel;
+    private JTextField fullNameTextField;
+    private JLabel roleLabel;
+    private JComboBox roleComboBox;
+    private JLabel addressLabel;
 }
