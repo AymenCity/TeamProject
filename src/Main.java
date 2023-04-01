63cf76ec7689da5a1c9c2276ce1a58383a98031b
AymenCity <91978242+AymenCity@users.noreply.github.com>
2023-04-01 13:26:02 +0100

    Aymen - added database connectivity for both accounts

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 // Runs the main program
 
 public class Main {
@@ -7,5 +10,24 @@ public class Main {
         UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());     // setting the UI to be Metal
         System.out.println("Booting up... ATS System");    // terminal output test
         Login login = new Login();  // runs program
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://smcse-stuproj00.city.ac.uk:3306/in2018g21";
+            String user = "in2018g21_a";
+            String pass = "QOb9VqF7";
+            Connection con = DriverManager.getConnection(url,user,pass);
+            if (con!=null) {
+                System.out.println("Successful database connection");
+            }
+        }
+
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
