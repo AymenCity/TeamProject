5baa3a894fbe2c642f74e767d42905fe4457d8f8
AymenCity <91978242+AymenCity@users.noreply.github.com>
2023-04-02 17:51:13 +0100

    Aymen- Database of Ticket is loaded

--- src/StockControl.java
+++ src/StockControl.java
@@ -1,10 +1,9 @@
+import net.proteanit.sql.DbUtils;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.Date;
+import java.sql.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -34,8 +33,10 @@ public class StockControl extends JFrame {
     private JButton deleteButton;
     private JButton searchButton;
     private JTextField typeTextField;
+    private JScrollPane SPblankTable;
     Connection con;
     PreparedStatement pst;
+    Main main = new Main();
 
     public StockControl() {
         setContentPane(mainPanel);
@@ -44,8 +45,8 @@ public class StockControl extends JFrame {
         setVisible(true);
 
         // calling database connection from Main
-        Main main = new Main();
         main.connect();
+        load_table();
 
         // cancel button
         cancelButton.addActionListener(new ActionListener() {
@@ -60,17 +61,44 @@ public class StockControl extends JFrame {
         saveButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String blankID, type, number, price, status;
-                LocalDateTime flightDate = LocalDateTime.parse(dateTextField.getText(),
-                        DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+                String ticketBlankType, ticketBlankNumber, ticketPrice, ticketFlightDate, ticketFlightTime, ticketStatus;
+
+                ticketBlankType = typeTextField.getText();
+                ticketBlankNumber = numberTextField.getText();
+                ticketPrice = priceTextField.getText();
+                ticketFlightDate = dateTextField.getText();
+                ticketFlightTime = timeTextField.getText();
+                ticketStatus = statusTextField.getText();
+
+                /*LocalDateTime ticketFlightDate = LocalDateTime.parse(dateTextField.getText(),
+                    DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
                 LocalDateTime flightTime = LocalDateTime.parse(timeTextField.getText(),
-                        DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
-                blankID = blankIdTextField.getText();
-                type = typeTextField.getText();
-                number = numberTextField.getText();
-                price = priceTextField.getText();
-                status = statusTextField.getText();
+                    DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));*/
+
+
                 try {
+                    pst = main.con.prepareStatement("insert into Ticket(ticketBlankType, ticketBlankNumber, ticketPrice, ticketFlightDate, ticketFlightTime, ticketStatus)values(?,?,?,?,?,?)");
+                    pst.setString(1, ticketBlankType);
+                    pst.setString(2, ticketBlankNumber);
+                    pst.setString(3, ticketPrice);
+                    pst.setString(4, ticketFlightDate);
+                    pst.setString(5, ticketFlightTime);
+                    pst.setString(6, ticketStatus);
+                    pst.executeUpdate();
+                    JOptionPane.showMessageDialog(mainPanel, "Record Added");
+                    blankIdTextField.setText("");
+                    typeTextField.setText("");
+                    numberTextField.setText("");
+                    priceTextField.setText("");
+                    dateTextField.setText("");
+                    timeTextField.setText("");
+                    statusTextField.setText("");
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+
+
+               /* try {
 
                     pst = main.con.prepareStatement("insert into Ticket(ticketID,ticketBlankType, ticketBlankNumber, ticketPrice, ticketFlightDate, ticketFlightTime, ticketStatus)values(?,?,?,?,?,?,?)");
                     pst.setInt(1, 1);
@@ -98,10 +126,23 @@ public class StockControl extends JFrame {
 
                 } catch (Exception exception) {
                     exception.printStackTrace();
-                }
+                } */
 
             }
         });
+
+
+    }
+    void load_table() {
+        try {
+            pst = main.con.prepareStatement("select * from Ticket");
+            ResultSet rs = pst.executeQuery();
+            blankTable.setModel(DbUtils.resultSetToTableModel(rs));
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
