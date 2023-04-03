491f5f625b2085b591b7c0821d1a602b7d1a6b88
EthanBrewerCity <91800294+EthanBrewerCity@users.noreply.github.com>
2023-04-03 19:38:00 +0100

    Ethan Brewer - 03/04/23 - First full design of sale control implemented to look lik stock control

--- src/SaleControl.java
+++ src/SaleControl.java
@@ -1,25 +1,39 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SaleControl {
-    private JTextField TickSearchBox;
+    private JPanel mainPanel;
+    private JTextField searchTextField;
     private JButton searchButton;
     private JLabel TitleLabel;
     private JButton deleteButton;
     private JTable table1;
     private JLabel FlavourTextLabel;
-    private JTextField textField1;
+    private JTextField blankIDTextField;
     private JLabel BlankIDLabel;
-    private JTextField textField2;
+    private JTextField typeTextField;
     private JLabel typeLabel;
-    private JTextField textField3;
+    private JTextField numberTextField;
     private JLabel numberButton;
-    private JTextField textField4;
+    private JTextField flightDateTextField;
     private JLabel flightDateButton;
     private JLabel statusButton;
-    private JTextField textField5;
+    private JTextField statusTextField;
     private JButton saveButton;
     private JButton updateButton;
     private JButton cancelButton;
-    private JTextField textField6;
-    private JTextField textField7;
+    private JTextField priceTextField;
+    private JTextField flightTimeTextField;
+    private JLabel priceLabel;
+    private JLabel flightTimeLabel;
+    private Login login;
+
+    public SaleControl(){
+        this.login = login;
+        setContentPane(mainPanel);
+        setTitle("ATS System");          // name of software
+        setSize(600,600);   // window resolution
+        setVisible(true);
+    }
 }
