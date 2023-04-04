c926b0675ae6442aeee41c53d2693415737d5867
AymenCity <91978242+AymenCity@users.noreply.github.com>
2023-04-04 13:24:58 +0100

    Aymen - loads table of Air_Ticket_Sale onto SaleControl and can add new table

--- src/SaleControl.java
+++ src/SaleControl.java
@@ -1,6 +1,11 @@
+import net.proteanit.sql.DbUtils;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class SaleControl extends JFrame {
     private JPanel mainPanel;
@@ -32,36 +37,81 @@ public class SaleControl extends JFrame {
     private JLabel AgentIDLabel;
     private JLabel custIDLabel;
     private JScrollPane saleControlTable;
+    Connection con;
+    PreparedStatement pst;
+    Main main = new Main();
 
     public SaleControl(){
         setContentPane(mainPanel);
         setTitle("ATS System");          // name of software
         setSize(600,600);   // window resolution
         setVisible(true);
+
+        main.connect(); // calling database connection from Main
+        load_table();   // loads table from database Air_Ticket_Sale
+
+        // SEARCH button
         searchButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Search Button activated!");
             }
         });
+
+        // DELETE button
         deleteButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Delete Button activated!");
             }
         });
+
+        // SAVE button
         saveButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("Saved!");
+                String saleID, saleType, saleTotal, saleCommission, saleGrandTotal, saleInterlineCurrencyRate;
+
+                saleID = saleIDTextField.getText();
+                saleType = saleTypeTextField.getText();
+                saleTotal = saleTotalTextField.getText();
+                saleCommission = commisTextField.getText();
+                saleGrandTotal = grandTotTextField.getText();
+                saleInterlineCurrencyRate = currRateTextField.getText();
+
+                try {
+                    pst = main.con.prepareStatement("insert into Air_Ticket_Sale(saleID, saleType, saleTotal, saleCommission, saleGrandTotal, saleInterlineCurrencyRate)values(?,?,?,?,?,?)");
+                    pst.setString(1, saleID);
+                    pst.setString(2, saleType);
+                    pst.setString(3, saleTotal);
+                    pst.setString(4, saleCommission);
+                    pst.setString(5, saleGrandTotal);
+                    pst.setString(6, saleInterlineCurrencyRate);
+                    pst.executeUpdate();
+                    JOptionPane.showMessageDialog(mainPanel, "Record Added");
+                    load_table();
+                    saleIDTextField.setText("");
+                    saleTypeTextField.setText("");
+                    saleTotalTextField.setText("");
+                    commisTextField.setText("");
+                    grandTotTextField.setText("");
+                    currRateTextField.setText("");
+                    saleIDTextField.requestFocus();
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
             }
         });
+
+        // UPDATE button
         updateButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Information Updated!");
             }
         });
+
+        // CANCEL button
         cancelButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -69,5 +119,17 @@ public class SaleControl extends JFrame {
                 WelcomePage welcomePage = new WelcomePage();
             }
         });
+    }
+
+    void load_table() {
+        try {
+            pst = main.con.prepareStatement("select * from Air_Ticket_Sale");
+            ResultSet rs = pst.executeQuery();
+            table1.setModel(DbUtils.resultSetToTableModel(rs));
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
