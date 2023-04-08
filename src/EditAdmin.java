3d4f88ee691b7954684d369d206eb9064d5ec968
Rati Sturua <rati.sturua@city.ac.uk>
2023-04-08 18:27:36 +0100

    Rati Sturua - 08/04/2023 - Created Froms and Classes for EditTravelAgent, EditOfficeManager, EditAdmin.

---
+++ src/EditAdmin.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class EditAdmin {
+    private JPanel parentPanel;
+    private JPanel mainPanel;
+    private JLabel stockControlLabel;
+    private JLabel nameLabel;
+    private JLabel usernameLabel;
+    private JButton saveButton;
+    private JButton cancelButton;
+    private JTextField nameTextField;
+    private JTextField usernameTextField;
+    private JTextField passwordTextField;
+    private JScrollPane SPblankTable;
+    private JTable blankTable;
+    private JButton searchButton;
+    private JLabel infoLabel;
+    private JButton deleteButton;
+    private JTextField searchTextField;
+    private JButton updateButton;
+    private JLabel passwordLabel;
+}
---
+++ src/EditTravelAgent.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+
+public class EditTravelAgent {
+    private JPanel mainPanel;
+    private JLabel stockControlLabel;
+    private JLabel nameLabel;
+    private JLabel usernameLabel;
+    private JButton saveButton;
+    private JButton cancelButton;
+    private JTextField nameTextField;
+    private JTextField usernameTextField;
+    private JTextField passwordTextField;
+    private JScrollPane SPblankTable;
+    private JTable blankTable;
+    private JButton searchButton;
+    private JLabel infoLabel;
+    private JButton deleteButton;
+    private JTextField searchTextField;
+    private JButton updateButton;
+    private JLabel passwordLabel;
+    private JTextField addressTextField;
+    private JTextField emailTextField;
+    private JPanel parentPanel;
+    private JLabel emailLabel;
+    private JLabel addressLabel;
+}
