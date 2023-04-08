5507d4e2fc932993c103aa36c07f96c1c7068413
Rati Sturua <rati.sturua@city.ac.uk>
2023-04-08 18:29:33 +0100

    Rati Sturua - 08/04/2023 - Created Froms and Classes for EditTravelAgent, EditOfficeManager, EditAdmin.

---
+++ src/EditOfficeManager.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+public class EditOfficeManager {
+    private JPanel mainPanel;
+    private JLabel stockControlLabel;
+    private JLabel managerNameLabel;
+    private JLabel usernameLabel;
+    private JButton saveButton;
+    private JButton cancelButton;
+    private JTextField nameTextField;
+    private JTextField usernameTextField;
+    private JTextField passwordTextField;
+    private JScrollPane SPblankTable;
+    private JTable blankTable;
+    private JButton searchButton;
+    private JLabel infoLabel;
+    private JButton deleteButton;
+    private JTextField searchTextField;
+    private JButton updateButton;
+    private JLabel passwordLabel;
+    private JLabel addressLabel;
+    private JLabel emailLabel;
+    private JTextField addressTextField;
+    private JTextField emailTextField;
+    private JTextField phoneNumberTextField;
+    private JLabel phoneNUmberLabel;
+}
